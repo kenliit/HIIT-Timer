667c39077643c59f40566efa7f79d996fc410cdc
Ken Li <abc00gh@hotmail.com>
2020-05-30 23:38:18 -0600

    Clean up

---
+++ HIIT/Models/ISettingsModel.cs
@@ -0,0 +1,14 @@
+﻿namespace HIIT.Models
+{
+    public interface ISettingsModel
+    {
+        int Break { get; set; }
+        int Rounds { get; set; }
+        int TimeOff { get; set; }
+        int TimeOn { get; set; }
+
+        void ReadConfig();
+        void SaveConfig();
+        void DeleteConfig();
+    }
+}
